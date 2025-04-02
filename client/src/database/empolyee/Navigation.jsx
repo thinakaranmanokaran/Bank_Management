2bf5fd585d80b1e408ac3a9ba9bce7fcbfca658e
Thinakaran <thinakaranmanohar@yahoo.com>
2025-04-02 16:33:28 +0530

    Update Navigation.jsx

--- client/src/database/empolyee/Navigation.jsx
+++ client/src/database/empolyee/Navigation.jsx
@@ -3,8 +3,8 @@ const EmployeeNavigation = [
     { title: "Profile", link: "/employee/profile" },
     { title: "Deposit Applications", link: "/employee/deposit" },
     { title: "Loan Applications", link: "/employee/loan" },
-    { title: "CIBIL Score", link: "/employee/cibil-score" },
-    { title: "User Profile", link: "/employee/user-profile" },
+    // { title: "CIBIL Score", link: "/employee/cibil-score" },
+    // { title: "User Profile", link: "/employee/user-profile" },
     // { title: "Users", link: "/employee/users" },
     // { title: "User Search", link: "/employee/user-search" },
 ];
